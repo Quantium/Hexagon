8be5961453dbdf2757e4cf08ea5b342c90750188
Andrés González Aragón <andres@quantium.com.mx>
2022-06-11 19:31:22 -0500

    Full Hexagons round

--- pointy_hex_corner.py
+++ pointy_hex_corner.py
@@ -2,24 +2,30 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Point Class
 class Point:
     def __init__(self,x,y):
         self.x, self.y = x,y
     def __str__(self):
         return 'Point({self.x},{self.y})'.format(self=self)
 
+# Hexagon Class
 class Hexagon:
+    # Constructor
+    # Input: center, size, type of hexagon (pointy or flat)
     def __init__(self,center,size,type='pointy'):
         if type != 'pointy' and type != 'flat':
             raise Exception("Type values available are 'pointy' and 'flat'")
         self.c,self.s,self.type = center,size,type
 
+    # Calculate the corner of the hexagon
     def corner(self,i):
         deg = 30 if self.type == 'pointy' else 0
         angle_deg = 60 *  i - deg
         angle_rad = math.pi/180 * angle_deg
         return Point(self.c.x + self.s * math.cos(angle_rad), self.c.y + self.s * math.sin(angle_rad))
 
+    # Calculate all the corners of the hexagon
     def draw_out(self):
         x,y=[],[]
         for i in range(1,8):
@@ -38,24 +44,47 @@ class Hexagon:
 
 ix,iy,size=1,1,3
 
+# Center Hexagon
 p1 = Point(ix,iy)
 hex1 = Hexagon(p1,size)
 x,y = hex1.draw_out()
-plt.plot(x,y)
+plt.plot(x,y,color='blue')
 
+# Top Right Hexagon
 p2 = Point(ix+(math.sqrt(3)*size)/2,iy+size*1.5)
 hex2 = Hexagon(p2,size)
 x2,y2 = hex2.draw_out()
-plt.plot(x2,y2)
+plt.plot(x2,y2,color='red')
 
+# Right Hexagon
 p3 = Point(ix+(math.sqrt(3)*size),iy)
 hex3 = Hexagon(p3,size)
 x3,y3 = hex3.draw_out()
-plt.plot(x3,y3)
+plt.plot(x3,y3,color='green')
 
-p4 = Point(ix+(math.sqrt(3)*size)*1.5,iy+size*1.5)
+# Bottom Right Hexagon
+p4 = Point(ix+(math.sqrt(3)*size)/2,iy-size*1.5)
 hex4 = Hexagon(p4,size)
 x4,y4 = hex4.draw_out()
-plt.plot(x4,y4)
+plt.plot(x4,y4,color='yellow')
+
+# Bottom Left Hexagon
+p5 = Point(ix-(math.sqrt(3)*size)/2,iy-size*1.5)
+hex5 = Hexagon(p5,size)
+x5,y5 = hex5.draw_out()
+plt.plot(x5,y5,color='orange')
+
+# Left Hexagon
+p6 = Point(ix-(math.sqrt(3)*size),iy)
+hex6 = Hexagon(p6,size)
+x6,y6 = hex6.draw_out()
+plt.plot(x6,y6,color='purple')
+
+# Top Left Hexagon
+p7 = Point(ix-(math.sqrt(3)*size)/2,iy+size*1.5)
+hex7 = Hexagon(p7,size)
+x7,y7 = hex7.draw_out()
+plt.plot(x7,y7,color='black')
+
 
 plt.show()
