1ec717f22ea0ed04f42ab96f461b4c01eb537417
Andrés González Aragón <andres@quantium.com.mx>
2022-06-11 21:49:28 -0500

    Parametrization of the hexagons to draw as pointy⬢ or flat⬣

--- pointy_hex_corner.py
+++ pointy_hex_corner.py
@@ -36,7 +36,7 @@ class Hexagon:
             y.append(self.corner(i).y)
         return x,y
 
-    def draw_star(self):
+    def star(self):
         x,y=[self.c.x],[self.c.y]
         for i in range(1,8):
             x.append(self.corner(i).x)
@@ -46,69 +46,94 @@ class Hexagon:
         return x,y
 
     def top_right_hex_center(self):
-        return Point(self.c.x + (math.sqrt(3)*self.s)/2,self.c.y+self.s*1.5)
+        a,b = (self.c.x + (math.sqrt(3)*self.s)/2,self.c.y+self.s*1.5)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
 
     def bottom_right_hex_center(self):
-        return Point(self.c.x + (math.sqrt(3)*self.s)/2,self.c.y-self.s*1.5)
+        a,b = (self.c.x + (math.sqrt(3)*self.s)/2,self.c.y-self.s*1.5)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
     
     def bottom_left_hex_center(self):
-        return Point(self.c.x - (math.sqrt(3)*self.s)/2,self.c.y-self.s*1.5)
+        a,b=(self.c.x - (math.sqrt(3)*self.s)/2,self.c.y-self.s*1.5)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
 
     def top_left_hex_center(self):
-        return Point(self.c.x - (math.sqrt(3)*self.s)/2,self.c.y+self.s*1.5)
+        a,b=(self.c.x - (math.sqrt(3)*self.s)/2,self.c.y+self.s*1.5)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
 
     def right_hex_center(self):
-        return Point(self.c.x + (math.sqrt(3)*self.s),self.c.y)
+        a,b=(self.c.x + (math.sqrt(3)*self.s),self.c.y)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
 
     def left_hex_center(self):
-        return Point(self.c.x - (math.sqrt(3)*self.s),self.c.y)
+        a,b=(self.c.x - (math.sqrt(3)*self.s),self.c.y)
+        if self.type == 'pointy':
+            return Point(a,b)
+        elif self.type == 'flat':
+            return Point(b,a)
+    
+    def __str__(self):
+        return 'Hexagon({c},{p})'.format(c=self.c,p=self.points())
 
 
-# Initial center and size of the first hexagon (in the center)
-ix,iy,size=1,1,3
+# Initial center of the first hexagon (in the center), and size and type of all the hexagons
+ix,iy,size,t=1,1,3,'pointy'
 
 # Center Hexagon
 p1 = Point(ix,iy)
-hex1 = Hexagon(p1,size)
+hex1 = Hexagon(p1,size,t)
 x,y = hex1.points()
 plt.plot(x,y,color='blue')
 
 # Top Right Hexagon
 p2 = hex1.top_right_hex_center()
-hex2 = Hexagon(p2,size)
+hex2 = Hexagon(p2,size,t)
 x2,y2 = hex2.points()
 plt.plot(x2,y2,color='red')
 
 # Right Hexagon
 p3 = hex1.right_hex_center()
-hex3 = Hexagon(p3,size)
+hex3 = Hexagon(p3,size,t)
 x3,y3 = hex3.points()
 plt.plot(x3,y3,color='green')
 
 # Bottom Right Hexagon
 p4 = hex1.bottom_right_hex_center()
-hex4 = Hexagon(p4,size)
+hex4 = Hexagon(p4,size,t)
 x4,y4 = hex4.points()
 plt.plot(x4,y4,color='yellow')
 
 # Bottom Left Hexagon
 p5 = hex1.bottom_left_hex_center()
-hex5 = Hexagon(p5,size)
+hex5 = Hexagon(p5,size,t)
 x5,y5 = hex5.points()
 plt.plot(x5,y5,color='orange')
 
 # Left Hexagon
-# p6 = Point(ix-(math.sqrt(3)*size),iy)
 p6 = hex1.left_hex_center()
-hex6 = Hexagon(p6,size)
+hex6 = Hexagon(p6,size,t)
 x6,y6 = hex6.points()
 plt.plot(x6,y6,color='purple')
 
 # Top Left Hexagon
 p7 = hex1.top_left_hex_center()
-hex7 = Hexagon(p7,size)
+hex7 = Hexagon(p7,size,t)
 x7,y7 = hex7.points()
 plt.plot(x7,y7,color='black')
-
 
 plt.show()
