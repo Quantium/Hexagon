7fe76b89f0c0163ee7b7c2237ca88adaafdbbb4f
Andrés González Aragón <andres@quantium.com.mx>
2022-06-13 21:13:05 -0500

    🎢 Separation of the main and the Classes

--- pointy_hex_corner.py
+++ pointy_hex_corner.py
@@ -1,8 +1,5 @@
 import math
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 # Point Class
 class Point:
     def __init__(self,x,y):
@@ -34,15 +31,6 @@ class Hexagon:
         for i in range(1,8):
             x.append(self.corner(i).x)
             y.append(self.corner(i).y)
-        return x,y
-
-    def star(self):
-        x,y=[self.c.x],[self.c.y]
-        for i in range(1,8):
-            x.append(self.corner(i).x)
-            y.append(self.corner(i).y)
-            x.append(self.c.x)
-            y.append(self.c.y)
         return x,y
 
     def top_right_hex_center(self):
@@ -91,49 +79,3 @@ class Hexagon:
         return 'Hexagon({c},{p})'.format(c=self.c,p=self.points())
 
 
-# Initial center of the first hexagon (in the center), and size and type of all the hexagons
-ix,iy,size,t=1,1,3,'pointy'
-
-# Center Hexagon
-p1 = Point(ix,iy)
-hex1 = Hexagon(p1,size,t)
-x,y = hex1.points()
-plt.plot(x,y,color='blue')
-
-# Top Right Hexagon
-p2 = hex1.top_right_hex_center()
-hex2 = Hexagon(p2,size,t)
-x2,y2 = hex2.points()
-plt.plot(x2,y2,color='red')
-
-# Right Hexagon
-p3 = hex1.right_hex_center()
-hex3 = Hexagon(p3,size,t)
-x3,y3 = hex3.points()
-plt.plot(x3,y3,color='green')
-
-# Bottom Right Hexagon
-p4 = hex1.bottom_right_hex_center()
-hex4 = Hexagon(p4,size,t)
-x4,y4 = hex4.points()
-plt.plot(x4,y4,color='yellow')
-
-# Bottom Left Hexagon
-p5 = hex1.bottom_left_hex_center()
-hex5 = Hexagon(p5,size,t)
-x5,y5 = hex5.points()
-plt.plot(x5,y5,color='orange')
-
-# Left Hexagon
-p6 = hex1.left_hex_center()
-hex6 = Hexagon(p6,size,t)
-x6,y6 = hex6.points()
-plt.plot(x6,y6,color='purple')
-
-# Top Left Hexagon
-p7 = hex1.top_left_hex_center()
-hex7 = Hexagon(p7,size,t)
-x7,y7 = hex7.points()
-plt.plot(x7,y7,color='black')
-
-plt.show()
